02af3485ea0815eab5df8a428c56774952dea0ba
Lawson <lgbeaty@live.ca>
2017-04-11 13:34:50 +0300

    test linter push fails

--- database/db_build.js
+++ database/db_build.js
@@ -4,7 +4,7 @@ const dbConnection = require('./db_connection.js');
 
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
-dbConnection.query(sql, (error,res) => {
+dbConnection.query(sql, (error, res) => {
   if(error) {
     console.log(error);
     return error;
