e27935e590bb9b46e60a510579f91ac5456d3352
Lawson <lgbeaty@live.ca>
2017-04-11 14:32:46 +0300

    Lint db_build.js

--- database/db_build.js
+++ database/db_build.js
@@ -5,11 +5,9 @@ const dbConnection = require('./db_connection.js');
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 dbConnection.query(sql, (error, res) => {
-  if(error) {
+  if (error) {
     console.log(error);
     return error;
   }
-  else {
-    console.log('Kitty table created with result:', res);
-  }
+  return console.log('Kitty table created with result:', res);
 });
